e710c7339eb6d3b91b7ebff7378de94b8230c032
Samaan Sayed <nasifasayed@gmail.com>
2025-02-24 14:49:04 +0530

    added something

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>THIS IS AN TEST APPPPPPPPPPPPPPPPP</h1>
+    </>
+  );
+}
+
+export default App;
